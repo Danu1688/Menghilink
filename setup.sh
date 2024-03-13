2c4c3590d8d26386b5e4a9ed4896ba3f5215a280
Danu1688 <99875819+Danu1688@users.noreply.github.com>
2024-03-13 23:22:16 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/ash
-# Installation script by ARYO.
+# Installation script by Danu1688
 
 DIR=/usr/bin
 
@@ -12,7 +12,7 @@ clear
     echo "Untuk Menjalankan Ketik m dan enter di terminal"
     sleep 3
     echo ""
-    echo "SALAM SEDULURAN"
+    echo "SALAM SEDULURAN SAKLAWASE"
     echo ""
     echo ""
 }
@@ -21,15 +21,15 @@ download_files()
 {
     	clear
   	echo "Downloading files from repo hilink mak cling..."
-   	wget -O $DIR/m https://raw.githubusercontent.com/aryobrokolly/menghilink/main/usr/bin/m && chmod +x $DIR/m
- 	wget -O $DIR/hilink https://raw.githubusercontent.com/aryobrokolly/menghilink/main/usr/bin/hilink && chmod +x $DIR/hilink
-  	wget -O $DIR/balong-nvtool https://raw.githubusercontent.com/aryobrokolly/menghilink/main/usr/bin/balong-nvtool && chmod +x $DIR/balong-nvtool
-   	wget -O $DIR/updatehilink https://raw.githubusercontent.com/aryobrokolly/menghilink/main/usr/bin/balong-nvtool && chmod +x $DIR/updatehilink
+   	wget -O $DIR/m https://raw.githubusercontent.com/Danu1688/Menghilink/main/usr/bin/m && chmod +x $DIR/m
+ 	wget -O $DIR/hilink https://raw.githubusercontent.com/Danu1688/Menghilink/main/usr/bin/hilink && chmod +x $DIR/hilink
+  	wget -O $DIR/balong-nvtool https://raw.githubusercontent.com/Danu1688/Menghilink/main/usr/bin/balong-nvtool && chmod +x $DIR/balong-nvtool
+   	wget -O $DIR/updatehilink https://raw.githubusercontent.com/Danu1688/Menghilink/main/usr/bin/balong-nvtool && chmod +x $DIR/updatehilink
     	finish
 }
 
 echo ""
-echo "Install Script code from repo aryo."
+echo "Install Script code from repo Danu1688."
 
 while true; do
     read -p "This will download the files into $DIR. Do you want to continue (y/n)? " yn
